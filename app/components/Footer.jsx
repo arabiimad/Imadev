8b4d63227bafe66bfa86e3eee57e7ddb495f9c40
Imad eddine Arabi <arabi.imadeddine@gmail.com>
2023-10-30 12:05:55 +0100

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-12 flex flex-col md:flex-row items-center md:justify-center md:space-x-4">
         <span>Imadev</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <p className="hidden md:block">|</p>
+        <p className="text-slate-600">All rights reserved.©</p>
       </div>
     </footer>
   );
